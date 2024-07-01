173e861a589d2f9295fad263ba19110ee38487aa
SimionReshetnikov <abobus2543@gmail.com>
2024-07-01 16:44:37 +0500

    Add files via upload

---
+++ OtusHomeWork2/ICollections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace OtusHomeWork2
+{
+    public interface ICollections
+    {
+        public Stopwatch stopwatch { get; set; }
+        public Random random { get; }
+        public TimeSpan tWorks { get; set; }
+
+        public void AddValuesInCollection();
+
+        public void FindingAnItemInACollection();
+
+        public void DivisionElementsInto777();
+
+    }
+}
