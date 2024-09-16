55eda99b50383e156f4857a7bba8b5fcaf12dc50
SemionReshetnikov <abobus2543@gmail.com>
2024-09-16 12:52:29 +0300

    Add files via upload
    Описание/Пошаговая инструкция выполнения домашнего задания:
    1. Напишите класс ImageDownloader. В этом классе должен быть метод Download, который скачивает картинку из интернета. Для загрузки картинки можно использовать примерно такой код: 
    --------------------------------------------------------------
    // Откуда будем качать
    string remoteUri = "https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg";
    // Как назовем файл на диске
    string fileName = "bigimage.jpg";
    // Качаем картинку в текущую директорию
    var myWebClient = new WebClient();
    Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
    myWebClient.DownloadFile(remoteUri, fileName);
    Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
    ----------------------------------------------------------------------------
    2. Создайте экземпляр этого класса и вызовите скачивание большой картинки, например, https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg
    В конце работы программы выведите в консоль "Нажмите любую клавишу для выхода" и ожидайте нажатия любой клавиши.
    3. Добавьте события: в классе ImageDownloader в начале скачивания картинки и в конце скачивания картинки выкидывайте события (event) ImageStarted и ImageCompleted соответственно.
    В основном коде программы подпишитесь на эти события, а в обработчиках их срабатываний выводите соответствующие уведомления в консоль: "Скачивание файла началось" и "Скачивание файла закончилось".
    4. Сделайте метод ImageDownloader.Download асинхронным. Если Вы скачивали картинку с использованием WebClient.DownloadFile, то используйте теперь WebClient.DownloadFileTaskAsync - он возвращает Task.
    В конце работы программы выводите теперь текст "Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания" и ожидайте нажатия любой клавиши. Если нажата клавиша "A" - выходите из программы. В противном случае выводите состояние загрузки картинки (True - загружена, False - нет). Проверить состояние можно через вызов Task.IsCompleted.
    Поздравляю! Ваша загрузка картинки работает асинхронно с основным потоком консоли.
    5. Модифицируйте программу таким образом, чтобы она скачивала сразу 10 картинок одновременно, останавливая одновременно все потоки по нажатию кнопки "A". По нажатию других кнопок выводить на экран список загружаемых картинок и их состояния - загружены или нет, аналогично выводу для одной картинки.

---
+++ OtusHomework10/OtusHomework10/ImageDownloader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OtusHomework10
+{
+    internal class ImageDownloader
+    {
+        public List<Task> tasks { get; set; }
+        public List<string> FileNames { get; set; }
+        public event Action ImageStarted;
+        public event Action ImageCompleted;
+
+        public ImageDownloader(List<string> fileNames)
+        {
+            FileNames = fileNames;
+        }
+
+        /// <summary>
+        /// Создаем экземпляр класса WebClient 
+        /// и асинхнонно скачиваем картинку с заданного адреса
+        /// </summary>
+        /// <param name="fileName"> Имя файла (можно указать путь, куда сохранится файл) </param>
+        /// <param name="remoteUri"> Адрес, с которого скачивается файл</param>
+        /// <returns></returns>
+        private async Task Download(string fileName, string remoteUri)
+        {
+            var myWebClient = new WebClient();
+            ImageStarted();
+            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            ImageCompleted();
+        }
+
+        /// <summary>
+        /// Добавляем все Task в лист tasks
+        /// await Task.WhenAll(tasks) - ожидаем, когда все Task выполнятся
+        /// </summary>
+        /// <returns></returns>
+        public async Task DownloadTaskRun()
+        {
+            tasks = new List<Task>();
+            for(int i = 0; i < FileNames.Count; i++)
+            {
+                tasks.Add(Download(FileNames[i],
+                    "https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg"));
+            }
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ OtusHomework10/OtusHomework10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OtusHomework10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+    
+            List<string> list = new List<string>();
+            var randomNumber = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                list.Add($"C:\\Users\\kiram\\Desktop\\Учеба программированию\\newPhoto{randomNumber.Next(1000)}.jpg");
+            }
+
+            var imageDownloader = new ImageDownloader(list);
+
+            imageDownloader.ImageStarted +=
+                () => Console.WriteLine("Скачивание файла началось.");
+
+            imageDownloader.ImageCompleted +=
+                () => Console.WriteLine("Скачивание файла закончилось.");
+            
+            var initialized = imageDownloader.DownloadTaskRun();
+            
+
+            Console.WriteLine("Нажмите клавишу \"А\" для выхода или" +
+                "любую другую клавишу для проверки статуса скачивания.");
+
+            while (true)
+            {
+                if (Console.ReadKey().Key != ConsoleKey.A)
+                {
+                    for(int i = 0; i < imageDownloader.tasks.Count; i++)
+                    {
+                        Console.WriteLine($"\n" +
+                            $"Картинка: {imageDownloader.FileNames[i]}" +
+                            $" Статус:{imageDownloader.tasks[i].IsCompleted}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nПрограмма завершена.");
+                    
+                    break;
+                }
+            }
+        }
+    }
+}
