a0d248e8b678cb68058ba9ce3c467fbba372291c
SemionReshetnikov <abobus2543@gmail.com>
2024-10-01 17:52:10 +0300

    Add files via upload

---
+++ OtusHomework10/OtusHomework10/ImageDownloader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OtusHomework10
+{
+    internal class ImageDownloader
+    {
+        public event Action? ImageStarted;
+        public event Action? ImageCompleted;
+
+        /// <summary>
+        /// Создаем экземпляр класса WebClient 
+        /// и асинхнонно скачиваем картинку с заданного адреса
+        /// </summary>
+        /// <param name="fileName"> Имя файла (можно указать путь, куда сохранится файл) </param>
+        /// <param name="remoteUri"> Адрес, с которого скачивается файл</param>
+        /// <returns></returns>
+        public async Task Download(string fileName, string remoteUri)
+        {
+            using (var myWebClient = new WebClient())
+            {
+                ImageStarted();
+                await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+                ImageCompleted();
+            }
+        }
+    }
+}
---
+++ OtusHomework10/OtusHomework10/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OtusHomework10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var token = cts.Token;
+            List<string> fileName = new List<string>();
+            List<Task> tasks = new List<Task>();
+            var randomNumber = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                fileName.Add($"C:\\Users\\kiram\\Desktop\\Учеба программированию\\newPhoto{randomNumber.Next(1000)}.jpg");
+            }
+
+            var imageDownloader = new ImageDownloader();
+
+            imageDownloader.ImageStarted +=
+                () => Console.WriteLine("Скачивание файла началось.");
+
+            imageDownloader.ImageCompleted +=
+                () => Console.WriteLine("Скачивание файла закончилось.");
+            
+            var initialized = (DownloadTaskRun(tasks, fileName, imageDownloader), token);
+
+            Console.WriteLine("Нажмите клавишу \"А\" для выхода или" +
+                "любую другую клавишу для проверки статуса скачивания.");
+
+            while (true)
+            {
+                if (Console.ReadKey().Key != ConsoleKey.A)
+                {
+                    for(int i = 0; i < fileName.Count; i++)
+                    {
+                        Console.WriteLine($"\n" +
+                            $"Картинка: {fileName[i]}" +
+                            $" Статус:{tasks[i].IsCompleted}");
+                    }
+                }
+                else
+                {
+                    cts.Cancel();
+                    break;
+                }
+            }
+
+            if (initialized.token.IsCancellationRequested)
+            {
+                Console.WriteLine("\nПрограмма остановлена.");
+            }
+
+            cts.Dispose();
+        }
+
+        /// <summary>
+        /// Добавляем все Task в лист tasks
+        /// await Task.WhenAll(tasks) - ожидаем, когда все Task выполнятся
+        /// </summary>
+        /// <returns></returns>
+
+        public static async Task DownloadTaskRun(List<Task> tasks, List<string> list, ImageDownloader imageDownloader)
+        {
+            
+            for (int i = 0; i < list.Count; i++)
+            {
+                tasks.Add(imageDownloader.Download(list[i],
+                    "https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg"));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
