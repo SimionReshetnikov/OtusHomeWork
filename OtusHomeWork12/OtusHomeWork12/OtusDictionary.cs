a897dc601fb5e9dc0c8935f17d8c428ef27579a3
SemionReshetnikov <abobus2543@gmail.com>
2024-10-02 18:25:36 +0300

    Add files via upload

---
+++ OtusHomeWork12/OtusHomeWork12/OtusDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHomeWork12
+{
+    internal class OtusDictionary
+    {
+        private string[] values = new string[32];
+
+        public int Key { get; set; }
+        public string? Value { get; set; }
+
+        //индексатор
+        public string this[int key]
+        {
+            get
+            {
+                if (key > values.Length)
+                {
+                    throw new ArgumentOutOfRangeException("Значения с таким ключем нет в коллекции");
+                }
+
+                if (values[key] == null)
+                {
+                    throw new EmptyPositionException("Пустая позиция");
+                }
+
+                return values[key];
+            }
+            set
+            {
+                if (values[key] != null)
+                {
+                    string[] newValues = new string[values.Length * 2];
+
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        newValues[i] = values[i];
+                    }
+
+                    values = newValues;
+
+                    throw new PositionTakenException("Позиция занята");
+                }
+
+                values[key] = value;
+            }
+        }
+
+
+        //Методы Add и Get
+        /*public void Add(int key, string value)
+        {
+            if (values[key] != null)
+            {
+                string[] newValues = new string[value.Length * 2];
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    newValues[i] = values[i];
+                }
+
+                values = newValues;
+
+                throw new PositionTakenException("Позиция занята");
+            }
+
+            values[key] = value;
+        }
+
+        public string Get(int key)
+        {
+            if(key > values.Length)
+            {
+                throw new ArgumentOutOfRangeException("Значения с таким ключем нет в коллекции");
+            }
+
+            if(values[key] == null)
+            {
+                throw new EmptyPositionException("Пустая позиция");
+            }
+
+            return values[key];
+        }*/
+
+    }
+}
---
+++ OtusHomeWork12/OtusHomeWork12/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace OtusHomeWork12
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int maxIndex = 31;
+            int countKollision = 0;
+            var dictionary = new OtusDictionary();
+
+            while (true)
+            {
+                Console.WriteLine("Введите номер нужной команды из списка:\n" +
+                    "1. Добавить значение;\n" +
+                    "2. Посмотреть значение по индексу;\n" +
+                    "Для выхода нажмите любую другую клавишу.");
+                
+                string result = Console.ReadLine();
+                
+                if(result != "1" && result != "2")
+                {
+                    Console.WriteLine("Выхожу из программы.");
+                    break;
+                }
+
+                if(result == "1")
+                {
+                    Console.WriteLine($"Введите индекс от 0 до {maxIndex}.");
+
+                    dictionary.Key = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите нужное значение.");
+
+                    dictionary.Value = Console.ReadLine();
+
+                    Console.WriteLine("Добывляю значения...");
+
+                    string message = "Значения успешно добавлены.";
+
+                    try
+                    {
+                        dictionary[dictionary.Key] = dictionary.Value;
+                        //dictionary.Add(dictionary.Key, dictionary.Value);
+                    }
+                    catch (PositionTakenException ex)
+                    {
+                        countKollision++;
+                        maxIndex = maxIndex*2 + countKollision;
+                        Console.WriteLine($"{ex.Message} максимальный индекс увеличен." +
+                            $" Доступны индексы для ввода от 0 до {maxIndex}");
+
+                        message = "Значение не добавлено. " +
+                            "Попробуйте указать другой индекс.";
+                    }
+                    finally
+                    {
+                        Console.WriteLine(message);
+                    }
+                }
+                else if (result == "2")
+                {
+                    Console.WriteLine("Введите индекс интересующего значения.");
+
+                    dictionary.Key = int.Parse(Console.ReadLine());
+
+                    try
+                    {
+                        Console.WriteLine($"По индексу {dictionary.Key} найдено значение {dictionary[dictionary.Key]}");
+                        //Console.WriteLine($"По индексу {dictionary.Key} найдено значение {dictionary.Get(dictionary.Key)}");
+                    }
+                    catch (EmptyPositionException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
