94bec36665a064fb81317a971a52ea71107a4428
SemionReshetnikov <abobus2543@gmail.com>
2024-10-15 18:55:07 +0300

    Add files via upload

---
+++ OtusHomeWork12/OtusDictionary.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHomeWork12
+{
+    internal class OtusDictionary
+    {
+        private int[] keysHashCode = new int[32];
+        private string[] values = new string[32];
+
+        public string this[int key]
+        {
+            get
+            {
+                if (key > values.Length)
+                {
+                    throw new ArgumentOutOfRangeException("Значения с таким ключем нет в коллекции");
+                }
+
+                if (values[key] == null)
+                {
+                    throw new EmptyPositionException("Пустая позиция");
+                }
+
+                return values[CalculationHashCode(key, keysHashCode.Length)];
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Строка не должна быть пустой");
+                }
+
+                if(key >=  values.Length)
+                {
+                    throw new IndexOutOfRangeException($"Вы можете добавить в коллекцию значение с индексом от 0 до {keysHashCode.Length}");
+                }
+
+                int hashCode = CalculationHashCode(key, keysHashCode.Length);
+
+                if (keysHashCode[key] == hashCode)
+                {
+                    string[] newValues = new string[values.Length * 2];
+                    int[] newKeysHashCode = new int[newValues.Length];
+
+                    for (int i = 0; i < keysHashCode.Length; i++)
+                    {
+                        newKeysHashCode[i] = CalculationHashCode(i, newKeysHashCode.Length);
+                        newValues[keysHashCode[i]] = values[i];
+                    }
+
+                    keysHashCode = newKeysHashCode;
+                    values = newValues;
+
+                    throw new PositionTakenException("Позиция занята");
+                }
+
+                keysHashCode[key] = hashCode;
+                values[hashCode] = value;
+            }
+        }
+        
+
+        public void Add(int key, string value)
+        {
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("Строка не должна быть пустой");
+            }
+
+            int hashCode = CalculationHashCode(key, keysHashCode.Length);
+
+            if (keysHashCode[key] == hashCode)
+            {
+                string[] newValues = new string[values.Length * 2];
+                int[] newKeysHashCode = new int[newValues.Length];
+
+                for(int i = 0; i < keysHashCode.Length; i++)
+                {
+                    newKeysHashCode[i] = CalculationHashCode(i, newKeysHashCode.Length);
+                }
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    newValues[keysHashCode[i]] = values[i];
+                }
+
+                keysHashCode = newKeysHashCode;
+                values = newValues;
+
+                throw new PositionTakenException("Позиция занята");
+            }
+
+            keysHashCode[key] = hashCode;
+            values[hashCode] = value;
+        }
+
+        public string Get(int key) => values[CalculationHashCode(key, keysHashCode.Length)];
+
+        private int CalculationHashCode(int key, int lengthBuckets)
+        {
+            return (key & 0x7fffffff) % lengthBuckets;
+        }
+    }
+}
---
+++ OtusHomeWork12/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+
+namespace OtusHomeWork12
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int maxIndex = 31;
+            int countKollision = 0;
+            var dictionary = new OtusDictionary();
+            int key;
+            var value = new StringBuilder();
+            bool сondition = true;
+
+            while (сondition)
+            {
+                Console.WriteLine("Введите номер нужной команды из списка:\n" +
+                    "1. Добавить значение;\n" +
+                    "2. Посмотреть значение по индексу;\n" +
+                    "Для выхода нажмите любую другую клавишу.");
+                
+                string result = Console.ReadLine();
+                
+                switch(result)
+                {
+                    case "1":
+
+                        Console.WriteLine($"Введите индекс от 0 до {maxIndex}.");
+
+                        key = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Введите нужное значение.");
+
+                        value.Append(Console.ReadLine());
+
+                        Console.WriteLine("Добывляю значения...");
+
+                        string message = "Значения успешно добавлены.";
+
+                        try
+                        {
+                            dictionary[key] = value.ToString();
+                        }
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        catch (ArgumentNullException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        catch (PositionTakenException ex)
+                        {
+                            countKollision++;
+                            maxIndex = maxIndex * 2 + countKollision;
+                            Console.WriteLine($"{ex.Message} максимальный индекс увеличен." +
+                                $" Доступны индексы для ввода от 0 до {maxIndex}");
+
+                            message = "Значение не добавлено. " +
+                                "Попробуйте указать другой индекс.";
+                        }
+                        finally
+                        {
+                            Console.WriteLine(message);
+                        }
+
+                        break;
+
+                    case "2":
+
+                        Console.WriteLine("Введите индекс интересующего значения.");
+
+                        key = int.Parse(Console.ReadLine());
+
+                        try
+                        {
+                            Console.WriteLine($"По индексу {key} найдено значение {dictionary[key]}");
+                        }
+                        catch (EmptyPositionException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Выхожу из программы.");
+                        сondition = false;
+
+                        break;
+                }
+                
+            }
+        }
+    }
+}
